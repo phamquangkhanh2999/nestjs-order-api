8824c74c36f6ba7b6c6f4fe410e13d678ab686c5
QuangKhanh <58257872+phamquangkhanh2999@users.noreply.github.com>
2025-06-04 10:18:51 +0700

    feat: update dto v2

--- src/modules/orders/entities/order.entity.ts
+++ src/modules/orders/entities/order.entity.ts
@@ -8,5 +8,11 @@ export interface Order {
   ward: string;
   address: string;
   product_note: string;
+  quantity: number;
+  utm_source: string;
+  utm_medium: string;
+  utm_campaign: string;
+  utm_content: string;
+  utm_term: string;
   created_at: string;
 }
